98aac569010c0754223d5ce4d575d5b12f4de5ae
Alasdair McLeay <alasdair.mcleay@me.com>
2022-11-12 16:19:27 +0000

    spring scale on hover

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Canvas, Color, Vector3 } from "@react-three/fiber";
 import { useRef, useState } from "react";
 import { EffectComposer, Bloom } from "@react-three/postprocessing";
 import { useControls } from "leva";
+import { useSpring, animated } from "@react-spring/three";
 
 function Box({
   keyNumber,
@@ -19,22 +20,24 @@ function Box({
   const mesh = useRef(null);
   const [hovered, setHover] = useState(false);
 
+  const { scale } = useSpring({ scale: hovered ? 0.97 : 1 });
   return (
-    <mesh
+    <animated.mesh
       position={position}
       ref={mesh}
+      scale={scale}
       onPointerDown={() => {
         playSound(keyNumber);
       }}
       onPointerEnter={() => setHover(true)}
       onPointerLeave={() => setHover(false)}
     >
-      <boxGeometry args={[1, 5, 1]} />
+      <boxGeometry args={[1, 5.5, 1]} />
       <meshBasicMaterial
         color={hovered ? activeColor : color}
         toneMapped={false}
       />
-    </mesh>
+    </animated.mesh>
   );
 }
 
@@ -57,13 +60,13 @@ function App() {
       </EffectComposer>
       <pointLight position={[10, 10, 10]} />
       <Box
-        position={[-3, 0, 0]}
+        position={[-2.5, 0, 0]}
         keyNumber={0}
         color={[0, 204 * (4 / 255), 255 * (4 / 255)]}
         active={[51 * (4 / 255), 214 * (4 / 255), 255 * (4 / 255)]}
       />
       <Box
-        position={[-1.5, 0, 0]}
+        position={[-1.25, 0, 0]}
         keyNumber={1}
         color={[255 * (4 / 255), 255 * (4 / 255), 0 * (4 / 255)]}
         active={[254 * (4 / 255), 255 * (4 / 255), 51 * (4 / 255)]}
@@ -75,13 +78,13 @@ function App() {
         active={[51 * (4 / 255), 255 * (4 / 255), 51 * (4 / 255)]}
       />
       <Box
-        position={[1.5, 0, 0]}
+        position={[1.25, 0, 0]}
         keyNumber={3}
         color={[250 * (4.5 / 255), 0 * (4.5 / 255), 150 * (4.5 / 255)]}
         active={[255 * (4.5 / 255), 47 * (4.5 / 255), 171 * (4.5 / 255)]}
       />
       <Box
-        position={[3, 0, 0]}
+        position={[2.5, 0, 0]}
         keyNumber={4}
         color={[0 * (4.3 / 255), 10 * (4.3 / 255), 770 * (4.3 / 255)]}
         active={[15 * (4.3 / 255), 90 * (4.3 / 255), 770 * (4.3 / 255)]}
