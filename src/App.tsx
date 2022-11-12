102af7b7a053d7542e2e13cfda612953a39df8cd
Alasdair McLeay <alasdair.mcleay@me.com>
2022-11-12 16:11:48 +0000

    Bloom

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import "./xylophone.css";
 import { playSound } from "./xylophone";
 import { Canvas, Color, Vector3 } from "@react-three/fiber";
 import { useRef, useState } from "react";
+import { EffectComposer, Bloom } from "@react-three/postprocessing";
+import { useControls } from "leva";
 
 function Box({
   keyNumber,
@@ -28,44 +30,61 @@ function Box({
       onPointerLeave={() => setHover(false)}
     >
       <boxGeometry args={[1, 5, 1]} />
-      <meshStandardMaterial color={hovered ? color : activeColor} />
+      <meshBasicMaterial
+        color={hovered ? activeColor : color}
+        toneMapped={false}
+      />
     </mesh>
   );
 }
 
 function App() {
+  const { intensity, radius } = useControls({
+    intensity: { value: 0.6, min: 0, max: 1.5, step: 0.01 },
+    radius: { value: 0.4, min: 0, max: 1, step: 0.01 },
+  });
   return (
-    <Canvas>
+    <Canvas orthographic camera={{ zoom: 100 }}>
+      <color attach="background" args={["#111"]} />
+      <EffectComposer>
+        <Bloom
+          luminanceThreshold={1}
+          intensity={intensity}
+          levels={9}
+          mipmapBlur
+          radius={radius}
+        />
+      </EffectComposer>
       <pointLight position={[10, 10, 10]} />
       <Box
-        position={[-2, 0, 0]}
+        position={[-3, 0, 0]}
         keyNumber={0}
-        color="#00ccff"
-        active="#33d6ff"
+        color={[0, 204 * (4 / 255), 255 * (4 / 255)]}
+        active={[51 * (4 / 255), 214 * (4 / 255), 255 * (4 / 255)]}
       />
       <Box
-        position={[-1, 0, 0]}
+        position={[-1.5, 0, 0]}
         keyNumber={1}
-        color="#ffff00"
-        active="#feff33"
+        color={[255 * (4 / 255), 255 * (4 / 255), 0 * (4 / 255)]}
+        active={[254 * (4 / 255), 255 * (4 / 255), 51 * (4 / 255)]}
       />
       <Box
         position={[0, 0, 0]}
         keyNumber={2}
-        color="#00ff00"
-        active="#33ff33"
+        color={[0 * (4 / 255), 255 * (4 / 255), 0 * (4 / 255)]}
+        active={[51 * (4 / 255), 255 * (4 / 255), 51 * (4 / 255)]}
       />
       <Box
-        position={[1, 0, 0]}
+        position={[1.5, 0, 0]}
         keyNumber={3}
-        color="#fa0096"
-        active="#ff2fab"
+        color={[250 * (4.5 / 255), 0 * (4.5 / 255), 150 * (4.5 / 255)]}
+        active={[255 * (4.5 / 255), 47 * (4.5 / 255), 171 * (4.5 / 255)]}
       />
       <Box
-        position={[2, 0, 0]}
+        position={[3, 0, 0]}
         keyNumber={4}
-        color="#0f4295"
-        active="#0059e9"
+        color={[0 * (4.3 / 255), 10 * (4.3 / 255), 770 * (4.3 / 255)]}
+        active={[15 * (4.3 / 255), 90 * (4.3 / 255), 770 * (4.3 / 255)]}
       />
     </Canvas>
   );
